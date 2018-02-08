27e3f4f3b6fb9d0708f37996a1452d0c38fd4d11
GOWTHAMAN7 <35653111+GOWTHAMAN7@users.noreply.github.com>
2018-02-08 16:23:05 +0530

    Create gfile123.c

---
+++ gfile123.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+    int  char1,char2;
+    printf("is a alphapet");
+    scanf("%d"&char1);
+    printf("is a not alphapet");
+    scanf("%d"&char2);
+    getch();
+}
+
