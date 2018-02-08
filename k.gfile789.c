632785f56228ac188177041f5e730f6dcddfaf6a
GOWTHAMAN7 <35653111+GOWTHAMAN7@users.noreply.github.com>
2018-02-08 16:34:48 +0530

    Create k.gfile789.c

---
+++ k.gfile789.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+void main()
+int main {		
+	int a,b,c;
+	printf("a is great");
+	scanf("%d%d%d",&a,&b,&c);
+	if(a>b &&)
+    else if(b>c)   a>c) 
+    printf("b is great");	
+    else
+    printf("c is great");
+	return 0;
+}
